0c6cd7f18eb292e7d32b33bc577fbfce2fd2e3d3
Kasim <kasimnadim6@gmail.com>
2021-09-16 09:22:16 +0530

    header button changes

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -2,11 +2,8 @@ import React from 'react';
 import classes from './Header.module.scss';
 import mealsImg from '../../assets/images/meals.jpeg';
 import HeaderCartButton from './HeaderCartButton';
-import { useContext } from 'react';
-import CartContext from '../../store/cart-context';
 
 const Header = ({ onShowCart }) => {
-  const cartCtx = useContext(CartContext);
   return (
     <>
       <header className={classes.header}>
--- src/components/Layouts/HeaderCartButton.js
+++ src/components/Layouts/HeaderCartButton.js
@@ -1,14 +1,36 @@
 import CartIcon from '../../assets/icons/CartIcon';
 import classes from './HeaderCartButton.module.scss';
+import { useContext, useEffect, useState } from 'react';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = ({ onClick }) => {
+  const [isButtonHighlighted, setIsButtonHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
+  const numberOfCartItems = cartCtx.items.reduce((cur, item) => {
+    return cur + item.amount;
+  }, 0);
+
+  const btnClasses = `${classes.button} ${
+    isButtonHighlighted ? classes.bump : ''
+  }`;
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setIsButtonHighlighted(true);
+    const timer = setTimeout(() => setIsButtonHighlighted(false), 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
   return (
-    <button className={classes.button} onClick={onClick}>
+    <button className={btnClasses} onClick={onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
